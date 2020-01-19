d7baf03196330c513d9afdbde6f000a3c99fb401
orenus <oren.sea@gmail.com>
2020-01-19 16:40:30 +0200

    bump version 0.0.8

--- python/setup.py
+++ python/setup.py
@@ -2,16 +2,11 @@
 
 from setuptools import setup
 
-# from setuptools import find_packages
-
 setup(
     name="configuration_client",
-    # packages=find_packages(exclude=['test*']),
     packages=["configuration_client"],
-    # py_modules='configuration_client',
     package_dir={"configuration_client": "."},
     package_data={"configuration_client": ["src/*"]},
-    # data_files=[('config', ['config/file'])],
     description="environment aware configuration module for Twist modules",
     classifiers=["Private :: Do Not Upload to pypi server"],
     install_requires=["hvac", "requests"],
