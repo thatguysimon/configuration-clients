b6af4f4b655e8b17e27b361c86444395e3f39c5c
orenus <oren.sea@gmail.com>
2020-01-19 14:27:22 +0200

    expr #3

--- python/setup.py
+++ python/setup.py
@@ -7,7 +7,8 @@ from setuptools import setup
 setup(
     name="configuration_client",
     # packages=find_packages(exclude=["test*"]),
-    py_modules="configuration_client",
+    packages=["configuration_client"],
+    # py_modules="configuration_client",
     package_dir={"configuration_client": "."},
     # package_data={"configuration_client": ["config/*"]},
     # include_package_data=True,
@@ -15,9 +16,10 @@ setup(
     description="environment aware configuration module for Twist modules",
     classifiers=["Private :: Do Not Upload to pypi server"],
     install_requires=["hvac", "requests"],
-    version="0.0.3",
-    url="http://github.com/twistbioscience/configuration-clients",
+    version="0.0.4",
+    url="git@github.com/twistbioscience/configuration-clients.git",
     author="Oren Sea",
     author_email="oren@prodops.io",
     keywords=["pip", "configuration", "twist", "packaging", "package"],
+    zip_safe=False,
 )
