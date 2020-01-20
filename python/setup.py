8400368f48b5003381af2056bb3e6999e5e000fa
orenus <oren.sea@gmail.com>
2020-01-20 16:00:37 +0200

    consum test #2

--- python/setup.py
+++ python/setup.py
@@ -5,8 +5,8 @@ from setuptools import setup
 setup(
     name="configuration_client",
     packages=["configuration_client"],
-    package_dir={"configuration_client": "."},
-    # package_data={"configuration_client": ["src/*"]},
+    package_dir={"configuration_client": "src"},
+    package_data={"configuration_client": ["src/*"]},
     description="environment aware configuration module for Twist modules",
     classifiers=["Private :: Do Not Upload to pypi server"],
     install_requires=["hvac", "requests"],
