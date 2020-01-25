57d56845907e50b7b35e78e792e4ae6cf2267b71
orenus <oren.sea@gmail.com>
2020-01-25 20:20:55 +0200

    drone step

--- node/src/kitchenSink.ts
+++ node/src/kitchenSink.ts
@@ -1,6 +1,5 @@
 import EnvConfig from './EnvConfig';
 
-
 // in order to run:
 // npm i
 // TWIST_ENV=kitchen-sink-demo-do-not-delete npm run demo
@@ -12,24 +11,20 @@ import EnvConfig from './EnvConfig';
 // visit https://github.com/Twistbioscience/configuration/tree/kitchen-sink-demo-do-not-delete
 // in order to examine concrete values involved in this example.
 
-
 async function main() {
-  // attempting to read the "all" section from the system.json conf file in the configuration repo
-  let v = await EnvConfig.get("system", 'all');
-  console.log(`got ${JSON.stringify(v)} from system conf [all] section...\n`);
+    // attempting to read the "all" section from the system.json conf file in the configuration repo
+    let v = await EnvConfig.get('system', 'all');
+    console.log(`got ${JSON.stringify(v)} from system conf [all] section...\n`);
 
-  // attempting to read the all.some_demo_key value from the system.json conf file in the configuration repo
-  v = await EnvConfig.get("system", "all", "some_demo_key")
-  console.log(`got "${v}" from system conf [all.some_demo_key]...\n`)
+    // attempting to read the all.some_demo_key value from the system.json conf file in the configuration repo
+    v = await EnvConfig.get('system', 'all', 'some_demo_key');
+    console.log(`got "${v}" from system conf [all.some_demo_key]...\n`);
 
-  // attempting to read a non existing config from the global.json conf file in the configuration repo
-  v = await EnvConfig.get(
-    "global",
-    "non_existing_section",
-    "non_existing_key",
-    ["this is a default value as a single list element"]
-  )
-  console.log(`got "${v}" from system conf global section...\n`)
+    // attempting to read a non existing config from the global.json conf file in the configuration repo
+    v = await EnvConfig.get('global', 'non_existing_section', 'non_existing_key', [
+        'this is a default value as a single list element',
+    ]);
+    console.log(`got "${v}" from system conf global section...\n`);
 }
 
-main()
+main();
