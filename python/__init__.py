de026c8e6afe2e5a02965b63603f4e321336dfa1
orenus <oren.sea@gmail.com>
2020-02-24 10:59:34 +0200

    exporting configBuilder

--- python/__init__.py
+++ python/__init__.py
@@ -5,5 +5,6 @@ this file EXPORTS the configuration_client package interface
 from src.env_config import EnvConfig
 from src.secrets import Secrets
 from src.os_vars import OSVars
+from src.config_builder import ConfigBuilder
 
-__all__ = ["EnvConfig", "Secrets", "OSVars"]
+__all__ = ["EnvConfig", "Secrets", "OSVars", "ConfigBuilder"]
