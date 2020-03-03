03b941f4300243fca1b2e813bcbedef17593b2aa
orenus <oren.sea@gmail.com>
2020-03-03 10:23:48 +0200

    exporting module interface

---
+++ node/main.ts
@@ -0,0 +1,7 @@
+import EnvConfig from './src/EnvConfig'
+import OSVars from './src/OSVars'
+import Secrets from './src/Secrets'
+
+exports.EnvConfig = EnvConfig;
+exports.OSVars = OSVars;
+exports.Secrets = Secrets;
