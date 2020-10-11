6cfa3fc6e48b85e8b844eeed8d318df18da58b2d
Orens <oren.sea@gmail.com>
2020-10-11 12:01:45 +0300

    freezing py deps

--- python/setup.py
+++ python/setup.py
@@ -10,7 +10,7 @@ setup(
     package_data={"configuration_client": ["src/*"]},
     description="environment aware configuration module for Twist modules",
     classifiers=["Private :: Do Not Upload to pypi server"],
-    install_requires=["hvac", "requests", "json5", "termcolor", "PyYAML"],
+    install_requires=["hvac==0.9.6", "requests==2.22.0", "json5==0.8.5", "termcolor===1.1.0", "PyYAML==5.3.1"],
     version="0.0.23",
     url="git@github.com/twistbioscience/configuration-clients.git",
     author="Oren Sea",
