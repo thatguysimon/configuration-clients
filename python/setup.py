01da81f9ca2f9214ed59678f7a277dad71f5c50e
Orenus <oren.sea@gmail.com>
2021-04-26 16:00:07 +0300

    bump version

--- python/setup.py
+++ python/setup.py
@@ -11,7 +11,7 @@ setup(
     description="environment aware configuration module for Twist modules",
     classifiers=["Private :: Do Not Upload to pypi server"],
     install_requires=["hvac", "requests", "json5==0.8.5", "termcolor", "PyYAML==5.3.1"],
-    version="0.0.30",
+    version="0.0.31",
     url="git@github.com/twistbioscience/configuration-clients.git",
     author="Oren Sea",
     author_email="osea@twistbioscience.com",
